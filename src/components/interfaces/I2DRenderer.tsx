208eadbdfa0e064b20cd6ec88528dbd2f5d39bbb
leeeentz <lintzmiriam@gmail.com>
2026-07-09 01:17:37 -0300

    removed I2DRenderer responsibility over managing objects

--- src/components/interfaces/I2DRenderer.tsx
+++ src/components/interfaces/I2DRenderer.tsx
@@ -1,11 +1,9 @@
 import IObject2D from "./IObject2D";
 
 interface I2DRenderer {
-    renderingObjectsBuffer: Array<IObject2D>;
-    displayObjects(viewContext: string): void;
+    displayObjects(viewContext: string, renderingObjectsBuffer: Array<IObject2D>): void;
     isInitialized: boolean;
 
     initialize(p: any):void;
-    pushObject2DToRenderingBuffer(obj: IObject2D): void;
 }
 export default I2DRenderer;
