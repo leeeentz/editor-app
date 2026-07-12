6f201a2f8cb4a0bdb0cc9d10e2ab64c7fc067735
leeeentz <lintzmiriam@gmail.com>
2026-07-12 17:40:26 -0300

    as ill be manipulating objects through indices, the destroyObjectByIndex() should return -1

--- src/components/interfaces/IObjects2DBuffer.tsx
+++ src/components/interfaces/IObjects2DBuffer.tsx
@@ -4,7 +4,7 @@ interface IObjects2DBuffer {
 	getObjectByIndex(index: number): IObject2D;
 	updateObjectByIndex(index: number, obj: IObject2D): void;
 	getObjectsArray(): Array<IObject2D>;
-	destroyObjectByIndex(index: number): void;
+	destroyObjectByIndex(index: number): number;
 	getIndexesSetAsArray(): Array<number>;
 	addObject(obj: IObject2D): number;
 }
